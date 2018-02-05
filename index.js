09703b4dd34b6c04c6ce47173dbd4a3d98fd87bc
Ivan Cabrera <ivancabrera931@gmail.com>
2018-02-05 16:37:19 -0300

    update
    fix array

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ app.get('/api', (req, res) => {
 
 app.post('/api/posts', verifyToken, (req, res) => {  
 
-  let db = {
-    "one":   {"email": "brad@gmail.com", "secret": "secrethyuj"},
-    "two":   {"email": "ivo@gmail.com", "secret": "koijuyhgs"},
-    "three": {"email": "pablo@gmail.com", "secret": "jouhsnh"}
-	}	
+  //let db = {
+  //  "one":   {"email": "brad@gmail.com", "secret": "secrethyuj"},
+  //  "two":   {"email": "ivo@gmail.com", "secret": "koijuyhgs"},
+  //  "three": {"email": "pablo@gmail.com", "secret": "jouhsnh"}
+	//}	
+
+  let db = [["brad@gmail.com","secret222"],
+            ["ivo@gmail.com", "secret"],
+            ["pablo@gmail.com", "secret3333"]];
 
   // get the decoded payload ignoring signature, no secretOrPrivateKey needed
   var decoded = jwt.decode(req.token);
@@ -26,10 +30,11 @@ app.post('/api/posts', verifyToken, (req, res) => {
   var decoded = jwt.decode(req.token, {complete: true});
   //console.log(decoded.header);
   //console.log(decoded.payload)
+ 
+  let id = decoded.payload.user.id; 
+  let secret = db[id][1]; 	
 
-  let id = decoded.payload.user.id;  	
-
-  jwt.verify(req.token, db[id].secret, (err, authData) => {
+  jwt.verify(req.token, secret, (err, authData) => {
     if(err) {
       res.sendStatus(403);
     } else {
@@ -44,12 +49,12 @@ app.post('/api/posts', verifyToken, (req, res) => {
 app.post('/api/login', (req, res) => {
   // Mock user
   const user = {
-    id: 'two', 
-    username: 'ivo',
-    email: 'ivo@gmail.com'
+    id: '0', 
+    username: 'Brad',
+    email: 'brad@gmail.com'
   }
 
-  jwt.sign({user}, 'koijuyhgs', { expiresIn: '1 day' }, (err, token) => {
+  jwt.sign({user}, 'secret222', (err, token) => {
     res.json({
       token
     });
